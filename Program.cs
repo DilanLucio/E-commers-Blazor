a73051948dd47448340ca060c93b34c1468d9feb
DilanLucio <dilan.lucio26@gmail.com>
2025-02-21 23:19:02 -0600

    Add of components Blazor

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DilanBlazor;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl)});
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
--- obj/Debug/net9.0/DilanBlazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/DilanBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DilanBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+065b58ca6120298ef299bc93c9eb006068ef277d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DilanBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DilanBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
